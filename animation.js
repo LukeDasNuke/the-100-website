86e46753f41344fe9c5c2bee0e3d7a02e5131f33
Freeranger <freeranger05@gmail.com>
2021-11-09 08:41:08 +0100

    Initial commit

---
+++ animation.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.trailer', {delay: 200, reset: true, duration: 1000, distance: '50px', origin: 'bottom'})
+ScrollReveal().reveal('.season-item', {delay: 375, reset: true, duration: 1000, distance: '50px', origin: 'bottom'})
+ScrollReveal().reveal('.info-left', {delay: 375, reset: true, duration: 1000, distance: '50px', origin: 'right'})
+ScrollReveal().reveal('.info-right', {delay: 375, reset: true, duration: 1000, distance: '50px', origin: 'left'})
